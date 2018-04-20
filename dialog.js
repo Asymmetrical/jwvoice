425e7af10bfb10097e88781b4e15bef55509abe1
Johan Wastring <jwr@SELUN-JWR-MAC1.lan>
2018-04-20 09:40:26 +0200

    No actions yet, but closer

--- dialog.js
+++ dialog.js
@@ -30,12 +30,12 @@ sessionClient
     console.log('DIALOG: Detected intent');
     const result = responses[0].queryResult;
     console.log(`DIALOG:   Query: ${result.queryText}`);
+    
     console.log(`DIALOG:   Response: ${result.fulfillmentText}`);
-    /*
-    // TODO - send to webpage to speak it 
-    var msg = new SpeechSynthesisUtterance(result.fulfillmentText);
-    window.speechSynthesis.speak(msg);
-    */
+
+// Check for not understanding    Response:activity: Can you say that again?
+    //console.log('Full result object: \n ' + JSON.stringify(result));
+
     if (result.intent) {
       console.log(`DIALOG:   Intent: ${result.intent.displayName}`);
       // if the intent is clear we can transform it into actions
@@ -56,6 +56,20 @@ var checkActionToPerform = (dialogflowResult) => {
   var currentIntent = dialogflowResult.intent.displayName;
   if (currentIntent === 'Navigate') {
     console.log('Navigate Logic');
+    console.log('Navigating to page: ' + dialogflowResult.parameters.fields.pages.stringValue);
+    //console.log(result.parameters);
+    // page navigate to this page
+    // below crashes if it is not in the 'navigate' intent
+    // not solid since reply can be other than containing parameters - fixed on dialogflow
+    //res.render('/' + result.parameters.fields.pages.stringValue);
+    // confirm that you will show the page
+    // meta from intent.other than displayname
+    // res.render(newPage) with data (this speech confirmation)
+    // TODO - send to webpage to speak it 
+    //var msg = new SpeechSynthesisUtterance(result.fulfillmentText);
+    //window.speechSynthesis.speak(msg);
+    // and possibly new actions
+    
   }
   if (currentIntent === 'Menu') {
     console.log('Menu Logic');
